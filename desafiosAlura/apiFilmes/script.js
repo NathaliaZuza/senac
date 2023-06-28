68534f192ac67d2ff73a753f6ce49694f2c9b17f
Nathália Zuza <nathalia555zuza@gmail.com>
2023-06-27 22:02:20 -0300

    dados estaticos

--- desafiosAlura/apiFilmes/script.js
+++ desafiosAlura/apiFilmes/script.js
@@ -1,9 +1,8 @@
-function renderMovie(movie) {
+function movieList(movie) {
     // Renderizar toda a estrutura do filme na página
 }
-const title = document.creatElement('section')
 
-title.classList.add('boxFilmes')
+const moviesContainer = document.querySelector('.container')
 
 const movies = [
   {
@@ -33,5 +32,69 @@ const movies = [
 ]
 
 window.onload = function() {
-    movies.forEach(movie => renderMovie(movie))
+    movies.forEach(movie =>  movieList(movie))
 }
+
+function movieList(movie) {
+
+    const { title, image, rating, year, description, isFavorited } = movie
+  
+    const movieElement = document.createElement('section')
+    movieElement.classList.add('boxFilmes')
+    moviesContainer.appendChild(movieElement)
+  
+    const movieInformations = document.createElement('div')
+    movieInformations.classList.add('boxFilmes__content')
+  
+    const movieImageContainer = document.createElement('div')
+    movieImageContainer.classList.add('contentImg')
+
+    const movieImage = document.createElement('img')
+    movieImage.src = image
+
+    movieImageContainer.appendChild(movieImage)
+    movieInformations.appendChild(movieImageContainer)
+  
+    const movieTextContainer = document.createElement('div')
+    movieTextContainer.classList.add('contentMain__info')
+    const movieTitle = document.createElement('h4')
+    movieTitle.textContent = `${title} (${year})`
+    movieTextContainer.appendChild(movieTitle)
+    movieInformations.appendChild(movieTextContainer)
+  
+    const informations = document.createElement('div')
+    informations.classList.add('favoritar')
+    movieTextContainer.appendChild(informations)
+  
+    const ratingContainer = document.createElement('div')
+    ratingContainer.classList.add('favoritar_estrela')
+    const starImage = document.createElement('img')
+    starImage.src = 'img/star.png'
+    const movieRate = document.createElement('span')
+    movieRate.classList.add('movie-rate')
+    movieRate.textContent = rating
+    ratingContainer.appendChild(starImage)
+    ratingContainer.appendChild(movieRate)
+    informations.appendChild(ratingContainer)
+  
+    const favorite = document.createElement('div')
+    favorite.classList.add('favoritar_coracao')
+    const favoriteImage = document.createElement('img')
+    favoriteImage.src = isFavorited ? 'img/heart-fill.svg' : 'img/heart.svg'
+    favoriteImage.classList.add('favoriteImage')
+    const favoriteText = document.createElement('span')
+    favoriteText.classList.add('movie-favorite')
+    favoriteText.textContent = 'Favoritar'
+    favorite.appendChild(favoriteImage)
+    favorite.appendChild(favoriteText)
+    informations.appendChild(favorite)
+  
+    const movieDescriptionContainer = document.createElement('div')
+    movieDescriptionContainer.classList.add('contentInfo')
+    const movieDescription = document.createElement('p')
+    movieDescription.textContent = description
+    movieDescriptionContainer.appendChild(movieDescription)
+    
+    movieElement.appendChild(movieInformations)
+    movieElement.appendChild(movieDescriptionContainer)
+  }
